b4fa3cbf94732aef4e70c791d422a0a87025f1cf
Bryant Vaughn <bf_vaughn@hotmail.com>
2019-06-03 16:54:14 -0600

    trying binary search

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-#import json
 import sys
 from web3 import Web3
-
-# print("This is the name of the script: ", sys.argv[0])
-# print("Number of arguments: ", len(sys.argv))
-# print("The arguments are: ", str(sys.argv))
 
 contract_address = sys.argv[1]
 infura_url = sys.argv[2]
 
 web3 = Web3(Web3.HTTPProvider(infura_url))
-bytecode = web3.eth.getCode(contract_address)
+
+low = 0
+high = web3.eth.blockNumber
+mid = high // 2
+
+while(low < high):
+	bytecode = web3.eth.getCode(account = contract_address, block_identifier = mid).hex()
+
+	# using binary approach to find first instance of contract
+	if bytecode == '0x': # contract doesn't exist yet at this block
+		low = mid
+		mid = (low + high) // 2
+	elif bytecode != '0x': # contract could 
+		high = mid
+		mid = (low + high) // 2
+
 print(bytecode)
